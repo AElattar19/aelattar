17985e600a07cb4c9a19ad56d2ddba0cac9cbcb7
AElattar <dr.aelattar@gmail.com>
2024-01-10 13:34:02 +0200

    10/1-14

--- resources/views/front/layouts/include/footer.blade.php
+++ resources/views/front/layouts/include/footer.blade.php
@@ -5,10 +5,10 @@
         <div class="col-lg-4 col-md-12 footer-info">
 
           <div class="social-links d-flex">
-            <a href="https://www.github.com/{{ $setting->github }}" class="twitter" target="_blank">github</a>
-            <a href="https://www.facebook.com/{{ $setting->facebook }}" class="facebook" target="_blank" >facebook</i></a>
-            <a href="https://www.youtube.com/{{ $setting->youtube }}" class="instagram" target="_blank">youtube</a>
-            <a href="https://www.linkedin.com/{{ $setting->linkedin }}" class="linkedin" target="_blank">linkedin</i></a>
+            <a href="https://www.github.com/{{ $setting->github }}" class="twitter" target="_blank">Github</a>
+            <a href="https://www.facebook.com/{{ $setting->facebook }}" class="facebook" target="_blank" >Facebook</i></a>
+            <a href="https://www.youtube.com/{{ $setting->youtube }}" class="instagram" target="_blank">Youtube</a>
+            <a href="https://www.linkedin.com/{{ $setting->linkedin }}" class="linkedin" target="_blank">Linkedin</i></a>
           </div>
         </div>
 
